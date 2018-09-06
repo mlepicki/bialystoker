9e487aec783de594a4ded4a46c2bc1c0c0422300
Marcin Lepicki <marcin.lepicki@gmail.com>
2018-09-06 15:41:58 +0200

    Added empty project

---
+++ Bialystoker/Bialystoker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Bialystoker
+//
+//  Created by Marcin Lepicki on 06/09/2018.
+//  Copyright © 2018 Marcin Łępicki. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
